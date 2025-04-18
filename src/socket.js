3d811a02bc4601c7e086bd79d4b7e03b7c9e66f4
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-04-18 23:41:37 +0530

    socket url chngd

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_SERVER_URL = "http://localhost:3001";
+const SOCKET_SERVER_URL = "https://realtime-chat-backend-bhec.onrender.com";
 
 const socket = io(SOCKET_SERVER_URL);
 
